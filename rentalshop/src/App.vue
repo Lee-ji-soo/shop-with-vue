f6bafb1e8d6f0d2a53a2308ce2ab5f3a00315fa9
Lee-ji-soo <nbsp3@naver.com>
2020-06-18 23:17:17 +0900

    install vue & bootstrap ready to start project !!

---
+++ rentalshop/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div>{{안녕}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      안녕: "hello"
+    };
+  },
+  components: {}
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
