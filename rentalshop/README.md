e3cd44e6e7e88ed5ee451a801e821f810f8ce88e
Lee-ji-soo <nbsp3@naver.com>
2020-06-23 19:54:13 +0900

    왼쪽필터바 , 오른쪽 상품리스트 반응형 정리

--- rentalshop/README.md
+++ rentalshop/README.md
@@ -3,6 +3,7 @@
 ## Project setup
 ```
 yarn install
+bootstrap
 ```
 
 ### Compiles and hot-reloads for development
@@ -22,3 +23,15 @@ yarn lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## Todolist
+
+#### 메인페이지
+- [x] vue 설치
+- [x] Bootstrap 설치
+- [x] 메인 css 정리
+- [x] 왼쪽필터바 (원래대로, 가격순, 가나다순) 반응형 문제 해결하기
+- [ ] 메인페이지에 정보 assets/data.js 에서 가져와서 뿌리기
+
+#### 상세페이지
+- [ ] 
--- rentalshop/src/App.vue
+++ rentalshop/src/App.vue
@@ -12,28 +12,34 @@
         </p>
       </b-jumbotron>
     </header>
-    <div class="body">
-      <div class="row">
-        <div id="filter" class="col-md-2">
-          <ul class="list-group">
-            <li class="list-group-item active">원래대로</li>
-            <li class="list-group-item">가격순</li>
-            <li class="list-group-item">가나다순</li>
-          </ul>
+    <div id="wrap">
+      <div id="filter">
+        <ul class="list-group">
+          <li class="list-group-item active">원래대로</li>
+          <li class="list-group-item">가격순</li>
+          <li class="list-group-item">가나다순</li>
+        </ul>
+      </div>
+      <div id="products">
+        <div class="list__box">
+          <img v-bind:src="room[0].img" />
+          <h5>{{room[0].title}}</h5>
+          <p>{{room[0].price}}</p>
         </div>
-        <div id="list" class="col-md-7">
-          <div class="row">
-            <div class="list__box col-md-6">
-              <img v-bind:src="room[0].img" />
-              <h5>{{room[0].title}}</h5>
-              <p>{{room[0].price}}</p>
-            </div>
-            <div class="list__box col-md-6">
-              <img v-bind:src="room[0].img" />
-              <h5>{{room[0].title}}</h5>
-              <p>{{room[0].price}}</p>
-            </div>
-          </div>
+        <div class="list__box">
+          <img v-bind:src="room[0].img" />
+          <h5>{{room[0].title}}</h5>
+          <p>{{room[0].price}}</p>
+        </div>
+        <div class="list__box">
+          <img v-bind:src="room[0].img" />
+          <h5>{{room[0].title}}</h5>
+          <p>{{room[0].price}}</p>
+        </div>
+        <div class="list__box">
+          <img v-bind:src="room[0].img" />
+          <h5>{{room[0].title}}</h5>
+          <p>{{room[0].price}}</p>
         </div>
       </div>
     </div>
@@ -55,51 +61,18 @@ export default {
 </script>
 
 <style>
+@import "./assets/css/index.css";
+@import "./assets/css/reset.css";
+
 * {
-  margin: 0;
-  padding: 0;
+  margin: 0px;
+  padding: 0px;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 0;
-}
-#header__tt {
-  font-size: 40px;
-  font-weight: 100;
-}
-
-.body {
-  margin: 0 50px;
-}
-
-.list-group .list-group-item.active {
-  background-color: #01cbae;
-  border: none;
-}
-
-#list {
-  min-width: 900px;
-}
-#list img {
-  width: 100%;
-}
-
-.row {
-  justify-content: space-evenly;
-}
-
-#list .row {
-  justify-content: start;
-}
-
-@media (max-width: 1140px) {
-  #list {
-    margin-top: 20px;
-    min-width: 90%;
-  }
 }
 </style>
